6363008d1de13671061bfbb39b14277479e22b44
eslamAbubakergaza <abuhashem.abu.baker@gmail.com>
2017-08-11 08:48:45 +0300

    eslam 11-8

---
+++ database/migrations/2017_05_23_043211_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->float('price');
+            $table->integer('contity');
+            $table->integer('sold');
+            $table->integer('category');
+
+            $table->string('img');
+            $table->integer('store_id');
+            $table->timestamps();
+            $table->foreign('store_id')
+                ->references('id')->on('stores')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
